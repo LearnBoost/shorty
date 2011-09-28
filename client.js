c27736dc902514cead19b80a74d7509c58fba852
Nathan Rajlich <nathan@tootallnate.net>
2011-09-27 17:04:22 -0700

    Don't need a DEFAULTS object.

--- client.js
+++ client.js
@@ -1,18 +1,15 @@
 var request = require('request')
 
-var DEFAULTS = {
-    method: 'POST'
-  , uri: 'https://'+ (process.env.SHORTY_DOMAIN || 'lrn.bt') + '/create'
-};
-
 module.exports = function client (url, opts, callback) {
   if ('function' == typeof opts) {
     callback = opts;
     opts = {};
   }
 
-  opts.__proto__ = DEFAULTS;
+  opts.method = 'POST';
   opts.json = { url: url };
+  if (!opts.uri)
+    opts.uri = 'https://'+ (process.env.SHORTY_DOMAIN || 'lrn.bt') + '/create';
 
   request(opts, function (err, res, body) {
     if (err) return callback(err);
