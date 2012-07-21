3c891aec94c4d3765026389878819c5a6ac8a687
Guillermo Rauch <rauchg@gmail.com>
2012-07-21 11:06:08 -0700

    Added redis config and simplified exports.

--- app.js
+++ app.js
@@ -22,262 +22,245 @@ var env = process.env.NODE_ENV || 'development';
  * Create db.
  */
 
-redis = require('redis').createClient();
+redis = require('redis').createClient(
+    process.env.SHORTY_REDIS_PORT
+  , process.env.SHORTY_REDIS_HOST
+);
 
 /**
- * App creator.
+ * Create app.
  */
 
-module.exports = (function (port, secure) {
+app = module.exports = express.createServer();
 
-  /**
-   * Create app.
-   */
+/**
+ * Basic middleware.
+ */
 
-  var app;
+if ('development' == env)  app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(stylus.middleware({ src: __dirname + '/public/', compile: css }));
+app.use(express.static(__dirname + '/public'));
 
-  if (secure) {
-    app = express.createServer({
-        key: read('ssl/' + env + '/key.key')
-      , cert: read('ssl/' + env + '/cert.crt')
-    });
-  } else {
-    app = express.createServer();
-  }
+/**
+ * Socket.IO
+ */
 
-  /**
-   * Basic middleware.
-   */
+var io = sio.listen(app);
 
-  if ('development' == env)  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(stylus.middleware({ src: __dirname + '/public/', compile: css }));
-  app.use(express.static(__dirname + '/public'));
+// quiet :)
 
-  /**
-   * Socket.IO
-   */
+io.set('log level', 0);
 
-  var io = sio.listen(app);
+/**
+ * Reads a file
+ *
+ * @api private
+ */
 
-  // quiet :)
+function read (file) {
+  return fs.readFileSync(__dirname + '/' + file, 'utf8');
+}
 
-  io.set('log level', 0);
+/**
+ * Stylus compiler
+ */
 
-  /**
-   * Reads a file
-   *
-   * @api private
-   */
+function css (str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', 'production' == env)
+    .use(nib())
+    .import('nib');
+};
 
-  function read (file) {
-    return fs.readFileSync(__dirname + '/' + file, 'utf8');
-  }
+/**
+ * Configure app.
+ */
 
-  /**
-   * Stylus compiler
-   */
+app.configure(function () {
+  app.set('views', __dirname);
+  app.set('view engine', 'jade');
+  app.set('domain', process.env.SHORTY_DOMAIN || 'lrn.cc');
+});
 
-  function css (str, path) {
-    return stylus(str)
-      .set('filename', path)
-      .set('compress', 'production' == env)
-      .use(nib())
-      .import('nib');
-  };
+/**
+ * Development configuration.
+ */
 
-  /**
-   * Configure app.
-   */
+app.configure('development', function () {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-  app.configure(function () {
-    app.set('views', __dirname);
-    app.set('view engine', 'jade');
-    app.set('domain', process.env.SHORTY_DOMAIN || 'lrn.cc');
+/**
+ * Production configuration.
+ */
+
+app.configure('production', function () {
+  app.use(express.errorHandler());
+});
+
+/**
+ * GET index page.
+ */
+
+app.get('/', function (req, res, next) {
+  redis.hlen('urls', function (err, length) {
+    if (err) return next(err);
+    res.render('index', { count: length });
   });
+});
 
-  /**
-   * Development configuration.
-   */
+/**
+ * POST a url.
+ */
 
-  app.configure('development', function () {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  });
+app.post('/', validate, exists, function (req, res, next) {
+  var url = req.body.url
+    , parsed = req.body.parsed
 
-  /**
-   * Production configuration.
-   */
+  // get count of urls
+  redis.hlen('urls', function (err, length) {
+    if (err) return next(500);
 
-  app.configure('production', function () {
-    app.use(express.errorHandler());
-  });
+    var short = base60.toString(length ? length + 1 : 0);
 
-  /**
-   * GET index page.
-   */
-
-  app.get('/', function (req, res, next) {
-    redis.hlen('urls', function (err, length) {
+    redis.hset('urls', short, url, function (err) {
       if (err) return next(err);
-      res.render('index', { count: length });
-    });
-  });
-
-  /**
-   * POST a url.
-   */
-
-  app.post('/', validate, exists, function (req, res, next) {
-    var url = req.body.url
-      , parsed = req.body.parsed
-
-    // get count of urls
-    redis.hlen('urls', function (err, length) {
-      if (err) return next(500);
-
-      var short = base60.toString(length ? length + 1 : 0);
-
-      redis.hset('urls', short, url, function (err) {
+      redis.hset('urls-hash', url, short, function (err) {
         if (err) return next(err);
-        redis.hset('urls-hash', url, short, function (err) {
-          if (err) return next(err);
 
-          var obj = {
-              type: 'url created'
-            , url: url
-            , short: short
-            , date: new Date
-          };
+        var obj = {
+            type: 'url created'
+          , url: url
+          , short: short
+          , date: new Date
+        };
 
-          redis.lpush('transactions', JSON.stringify(obj), function (err) {
-            if (err) return next(500);
+        redis.lpush('transactions', JSON.stringify(obj), function (err) {
+          if (err) return next(500);
 
-            obj.parsed = parsed;
-            io.of('/main').volatile.emit('total', length + 1);
-            io.of('/stats').volatile.emit('url created', short, parsed, Date.now());
-            res.send({ short: 'https://' + app.set('domain') + '/' + short });
-          });
+          obj.parsed = parsed;
+          io.of('/main').volatile.emit('total', length + 1);
+          io.of('/stats').volatile.emit('url created', short, parsed, Date.now());
+          res.send({ short: 'https://' + app.set('domain') + '/' + short });
         });
       });
     });
   });
+});
 
-  /**
-   * Checkes that the URL is valid
-   */
+/**
+ * Checkes that the URL is valid
+ */
 
-  function validate (req, res, next) {
-    var parsed = req.body.parsed = url.parse(req.body.url);
+function validate (req, res, next) {
+  var parsed = req.body.parsed = url.parse(req.body.url);
 
-    if (!req.body.url || !parsed.protocol || !parsed.host) {
-      return res.send(400, { error: 'Bad `url` field' });
-    }
+  if (!req.body.url || !parsed.protocol || !parsed.host) {
+    return res.send(400, { error: 'Bad `url` field' });
+  }
 
+  next();
+};
+
+/**
+ * Content negotiation.
+ */
+
+function accept(type) {
+  return function(req, res, next){
+    if (req.accepts(type)) return next();
+    next('route');
+  }
+}
+
+/**
+ * Checks that the URL doesnt exist already
+ */
+
+function exists (req, res, next) {
+  redis.hget('urls-hash', req.body.url, function (err, val) {
+    if (err) return next(err);
+    if (val) return res.send({ short: 'https://' + app.set('domain') + '/' + val });
     next();
-  };
+  });
+}
 
-  /**
-   * Content negotiation.
-   */
+/**
+ * GET JSON statistics.
+ */
 
-  function accept(type) {
-    return function(req, res, next){
-      if (req.accepts(type)) return next();
-      next('route');
-    }
-  }
+app.get('/stats', accept('json'), function (req, res, next) {
+  redis.lrange('transactions', 0, 100, function (err, vals) {
+    if (err) return next(err);
+    res.send(vals.map(JSON.parse));
+  });
+});
 
-  /**
-   * Checks that the URL doesnt exist already
-   */
+/**
+ * GET statistics.
+ */
 
-  function exists (req, res, next) {
-    redis.hget('urls-hash', req.body.url, function (err, val) {
-      if (err) return next(err);
-      if (val) return res.send({ short: 'https://' + app.set('domain') + '/' + val });
-      next();
+app.get('/stats', function (req, res, next) {
+  redis.lrange('transactions', 0, 100, function (err, vals) {
+    if (err) return next(err);
+    res.render('stats', { transactions: vals ? vals.map(function (v) {
+      v = JSON.parse(v);
+      v.parsed = url.parse(v.url);
+      delete v.url;
+      return v;
+    }).reverse() : [] });
+  });
+});
+
+/**
+ * GET :short url to perform redirect.
+ */
+
+app.get('/:short', function (req, res, next) {
+  redis.hget('urls', req.params.short, function (err, val) {
+    if (err) return next(err);
+    if (!val) return res.render('404');
+
+    redis.lpush('transactions', JSON.stringify({
+        type: 'url visited'
+      , url: val
+      , short: req.params.short
+      , date: Date.now()
+      , ip: req.socket.remoteAddress
+      , headers: req.headers
+    }), function (err) {
+      if (err) console.error(err);
     });
-  }
 
-  /**
-   * GET JSON statistics.
-   */
+    io.of('/stats').volatile.emit(
+        'url visited'
+      , req.params.short
+      , url.parse(val)
+      , Date.now()
+    );
 
-  app.get('/stats', accept('json'), function (req, res, next) {
-    redis.lrange('transactions', 0, 100, function (err, vals) {
-      if (err) return next(err);
-      res.send(vals.map(JSON.parse));
-    });
+    res.redirect(val);
+  });
+});
+
+/**
+ * Listen.
+ */
+
+if (!module.parent) {
+  app.listen(process.env.PORT || 3000, function () {
+    var addr = app.address();
+    console.error(
+        '   app listening on ' + addr.address + ':' + addr.port
+      + (secure ? ' (secure) ' : '')
+    );
   });
 
-  /**
-   * GET statistics.
-   */
-
-  app.get('/stats', function (req, res, next) {
-    redis.lrange('transactions', 0, 100, function (err, vals) {
-      if (err) return next(err);
-      res.render('stats', { transactions: vals ? vals.map(function (v) {
-        v = JSON.parse(v);
-        v.parsed = url.parse(v.url);
-        delete v.url;
-        return v;
-      }).reverse() : [] });
+  if (!process.listeners('uncaughtException')) {
+    process.on('uncaughtException', function (e) {
+      console.error(e && e.stack ? e.stack : e);
     });
-  });
-
-  /**
-   * GET :short url to perform redirect.
-   */
-
-  app.get('/:short', function (req, res, next) {
-    redis.hget('urls', req.params.short, function (err, val) {
-      if (err) return next(err);
-      if (!val) return res.render('404');
-
-      redis.lpush('transactions', JSON.stringify({
-          type: 'url visited'
-        , url: val
-        , short: req.params.short
-        , date: Date.now()
-        , ip: req.socket.remoteAddress
-        , headers: req.headers
-      }), function (err) {
-        if (err) console.error(err);
-      });
-
-      io.of('/stats').volatile.emit(
-          'url visited'
-        , req.params.short
-        , url.parse(val)
-        , Date.now()
-      );
-
-      res.redirect(val);
-    });
-  });
-
-  /**
-   * Listen.
-   */
-
-  if (!module.parent) {
-    app.listen(port, function () {
-      var addr = app.address();
-      console.error(
-          '   app listening on ' + addr.address + ':' + addr.port
-        + (secure ? ' (secure) ' : '')
-      );
-    });
-
-    if (!process.listeners('uncaughtException')) {
-      process.on('uncaughtException', function (e) {
-        console.error(e && e.stack ? e.stack : e);
-      });
-    }
   }
-
-  return arguments.callee;
-})
-('production' == env ? 80 : 3000)
-('production' == env ? 443 : 3001, true)
+}
